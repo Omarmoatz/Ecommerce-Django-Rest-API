b55ca11b686c01bb95a1c124482523097def9405
Omarmoatz <omar.raouf.002@gmail.com>
2024-01-22 17:09:18 +0200

    add JWT authentication

--- project/urls.py
+++ project/urls.py
@@ -16,11 +16,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('product/', include('product.urls')),
+    
+    # JWT Auth
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
 handler404 = 'utiles.error_handler.handler404'
