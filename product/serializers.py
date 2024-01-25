907d377470b9ad2c2fc1280d1931617d5043ad95
Omarmoatz <omar.raouf.002@gmail.com>
2024-01-25 16:56:40 +0200

    show all reviews in product list,detail

--- product/serializers.py
+++ product/serializers.py
@@ -3,9 +3,16 @@ from .models import Product,Review
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    reviews = serializers.SerializerMethodField(method_name='get_review')
+
     class Meta:
         model = Product
         fields = '__all__'
+
+    def get_review(self,obj):
+        review = obj.product_review.all()
+        serial = ReviewSerializer(review ,many=True).data
+        return serial
 
 class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
